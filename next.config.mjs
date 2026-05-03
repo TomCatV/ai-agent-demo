4a8591bdb61176df364a3d55dff31d9fdd58bfc9
dang <2363984977@qq.com>
2026-05-03 23:40:33 +0800

    细节

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,9 @@ const nextConfig = {
   devIndicators: false,
 };
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,4 +5,8 @@ const config = {
   },
 };
 
-export default config;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(config);
